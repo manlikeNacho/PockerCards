51ff33803e08e7ca23aedb553803b2d77da76b5e
Iheanacho Ebere <eiheanacho52@gmail.com>
2022-09-09 20:24:25 +0100

    Back

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,10 @@
-import adapter from '@sveltejs/adapter-node@next';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({ out: 'build' })
+		adapter: adapter()
 	},
 	preprocess: [
 		preprocess({
